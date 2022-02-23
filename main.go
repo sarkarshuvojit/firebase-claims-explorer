2b274476f8fd1ac9adc6cc3609e29f2903941c0c
Shuvojit <s15sarkar@yahoo.com>
2022-02-23 23:05:52 +0530

    Doc generation removed

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"github.com/spf13/cobra/doc"
 	"shuvojit.in/firebase-claims-explorer/cmd"
 )
 
 func main() {
-	err := doc.GenMarkdownTree(cmd.RootCmd, "./docs")
-	if err != nil {
-		panic(err)
-	}
 	cmd.Execute()
 }
