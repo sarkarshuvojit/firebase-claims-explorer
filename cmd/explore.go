d1bf5bb2c1c43d51733e4203d42fc32c9340fa2f
Shuvojit <s15sarkar@yahoo.com>
2023-04-07 02:24:43 +0530

    UI in progress; created columns

--- cmd/explore.go
+++ cmd/explore.go
@@ -5,14 +5,193 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 
 	"firebase.google.com/go/v4/auth"
 	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 	"shuvojit.in/firebase-claims-explorer/authentication"
 	"shuvojit.in/firebase-claims-explorer/components"
+)
+
+var (
+
+	// General.
+
+	subtle    = lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	highlight = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
+	special   = lipgloss.AdaptiveColor{Light: "#43BF6D", Dark: "#73F59F"}
+
+	divider = lipgloss.NewStyle().
+		SetString("•").
+		Padding(0, 1).
+		Foreground(subtle).
+		String()
+
+	url = lipgloss.NewStyle().Foreground(special).Render
+
+	// Tabs.
+
+	activeTabBorder = lipgloss.Border{
+		Top:         "─",
+		Bottom:      " ",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "┘",
+		BottomRight: "└",
+	}
+
+	tabBorder = lipgloss.Border{
+		Top:         "─",
+		Bottom:      "─",
+		Left:        "│",
+		Right:       "│",
+		TopLeft:     "╭",
+		TopRight:    "╮",
+		BottomLeft:  "┴",
+		BottomRight: "┴",
+	}
+
+	tab = lipgloss.NewStyle().
+		Border(tabBorder, true).
+		BorderForeground(highlight).
+		Padding(0, 1)
+
+	activeTab = tab.Copy().Border(activeTabBorder, true)
+
+	tabGap = tab.Copy().
+		BorderTop(false).
+		BorderLeft(false).
+		BorderRight(false)
+
+	// Title.
+
+	titleStyle = lipgloss.NewStyle().
+			MarginLeft(1).
+			MarginRight(5).
+			Padding(0, 1).
+			Italic(true).
+			Foreground(lipgloss.Color("#FFF7DB")).
+			SetString("Lip Gloss")
+
+	descStyle = lipgloss.NewStyle().MarginTop(1)
+
+	infoStyle = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderTop(true).
+			BorderForeground(subtle)
+
+	// Dialog.
+
+	dialogBoxStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#874BFD")).
+			Padding(1, 0).
+			BorderTop(true).
+			BorderLeft(true).
+			BorderRight(true).
+			BorderBottom(true)
+
+	buttonStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFF7DB")).
+			Background(lipgloss.Color("#888B7E")).
+			Padding(0, 3).
+			MarginTop(1)
+
+	activeButtonStyle = buttonStyle.Copy().
+				Foreground(lipgloss.Color("#FFF7DB")).
+				Background(lipgloss.Color("#F25D94")).
+				MarginRight(2).
+				Underline(true)
+
+	// List.
+
+	list = lipgloss.NewStyle().
+		Border(lipgloss.NormalBorder(), false, true, false, false).
+		BorderForeground(subtle).
+		MarginRight(2).
+		Height(8).
+		Width(columnWidth + 1)
+
+	listHeader = lipgloss.NewStyle().
+			BorderStyle(lipgloss.NormalBorder()).
+			BorderBottom(true).
+			BorderForeground(subtle).
+			MarginRight(2).
+			Render
+
+	listItem = lipgloss.NewStyle().PaddingLeft(2).Render
+
+	checkMark = lipgloss.NewStyle().SetString("✓").
+			Foreground(special).
+			PaddingRight(1).
+			String()
+
+	listDone = func(s string) string {
+		return checkMark + lipgloss.NewStyle().
+			Strikethrough(true).
+			Foreground(lipgloss.AdaptiveColor{Light: "#969B86", Dark: "#696969"}).
+			Render(s)
+	}
+
+	// Paragraphs/History.
+
+	historyStyle = lipgloss.NewStyle().
+			Align(lipgloss.Left).
+			Foreground(lipgloss.Color("#FAFAFA")).
+			Background(highlight).
+			Margin(1, 3, 0, 0).
+			Padding(1, 2).
+			Height(19).
+			Width(columnWidth)
+
+	// Status Bar.
+
+	statusNugget = lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Padding(0, 1)
+
+	statusBarStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{Light: "#343433", Dark: "#C1C6B2"}).
+			Background(lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#353533"})
+
+	statusStyle = lipgloss.NewStyle().
+			Inherit(statusBarStyle).
+			Foreground(lipgloss.Color("#FFFDF5")).
+			Background(lipgloss.Color("#FF5F87")).
+			Padding(0, 1).
+			MarginRight(1)
+
+	encodingStyle = statusNugget.Copy().
+			Background(lipgloss.Color("#A550DF")).
+			Align(lipgloss.Right)
+
+	statusText = lipgloss.NewStyle().Inherit(statusBarStyle)
+
+	fishCakeStyle = statusNugget.Copy().Background(lipgloss.Color("#6124DF"))
+
+	borderStyle = lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder()).
+			Render
+
+	// Page.
+
+	docStyle = lipgloss.NewStyle() //.Padding(1, 2, 1, 2)
+)
+
+const (
+	// In real life situations we'd adjust the document to fit the width we've
+	// detected. In the case of this example we're hardcoding the width, and
+	// later using the detected width only to truncate in order to avoid jaggy
+	// wrapping.
+	width = 96
+
+	columnWidth = 30
 )
 
 var globalClient *auth.Client
@@ -87,7 +266,9 @@ var (
 				panic(err)
 			}
 			m.textArea.SetValue(string(jsonString))
-			return m.textArea.View()
+			style := lipgloss.NewStyle().
+				MarginLeft(20)
+			return style.Render(m.textArea.View())
 		}}
 )
 
@@ -106,7 +287,6 @@ type exploreModel struct {
 func createExploreModel(users []authentication.User) exploreModel {
 
 	return exploreModel{
-		selectedScreen: LIST_SCREEN,
 
 		users:             users,
 		selectedUserIndex: 0,
@@ -127,13 +307,8 @@ func (m exploreModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
-			if m.selectedScreen.Name == LIST_SCREEN.Name {
-				clearScreen()
-				return m, tea.Quit
-			} else {
-				m.selectedScreen = LIST_SCREEN
-				break
-			}
+			clearScreen()
+			return m, tea.Quit
 		case "j", "down":
 			m.MoveCursorDown()
 			break
@@ -141,7 +316,6 @@ func (m exploreModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.MoveCursorUp()
 			break
 		case "enter":
-			m.ViewDetail()
 			break
 		}
 	}
@@ -167,7 +341,50 @@ func (m *exploreModel) MoveCursorUp() {
 	m.selectedUserIndex -= 1
 }
 
+func withTitle(title string, body string) string {
+	return borderStyle(lipgloss.JoinVertical(
+		lipgloss.Center,
+		listHeader(title),
+		body,
+	))
+}
+
 func (m exploreModel) View() string {
-	output := m.selectedScreen.Render(m)
-	return output
+	physicalWidth, _, _ := term.GetSize(int(os.Stdout.Fd()))
+	doc := strings.Builder{}
+	{
+		doc.WriteString(
+			lipgloss.JoinHorizontal(
+				lipgloss.Top,
+				withTitle("Users", LIST_SCREEN.Render(m)),
+				withTitle("Claims", DETAIL_SCREEN.Render(m)),
+			))
+		doc.WriteString("\n\n")
+	}
+	{
+		w := lipgloss.Width
+
+		statusKey := statusStyle.Render("STATUS")
+		encoding := encodingStyle.Render("UTF-8")
+		fishCake := fishCakeStyle.Render("🍥 Fish Cake")
+		statusVal := statusText.Copy().
+			Width(width - w(statusKey) - w(encoding) - w(fishCake)).
+			Render("Ravishing")
+
+		bar := lipgloss.JoinHorizontal(lipgloss.Top,
+			statusKey,
+			statusVal,
+			encoding,
+			fishCake,
+		)
+
+		doc.WriteString(statusBarStyle.Width(width).Render(bar))
+
+	}
+
+	if physicalWidth > 0 {
+		docStyle = docStyle.MaxWidth(physicalWidth)
+	}
+
+	return docStyle.Render(doc.String())
 }
--- go.mod
+++ go.mod
@@ -9,5 +9,6 @@ require (
 	github.com/charmbracelet/lipgloss v0.6.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cobra v1.3.0
+	golang.org/x/term v0.7.0 // indirect
 	google.golang.org/api v0.62.0
 )
